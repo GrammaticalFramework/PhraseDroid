a18faa43fe1c0f22d684e721b2bbfd4705a94f4f
Grégoire Détrez <gdetrez@crans.org>
2011-01-12 11:10:09 +0100

    Updates and graphics to get the app on the market

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+set -u
+set -v
+
+TEMPAPK=`tempfile -s .apk`
+KEYSTORE="$HOME/android-release-key.keystore"
+RELEASE="bin/PhraseDroid-release.apk"
+
+## Delete old release
+rm -f $RELEASE
+
+## build the unsigned apk
+ant release
+mv bin/PhraseDroid-unsigned.apk $TEMPAPK
+
+## sign the application with my release key
+jarsigner -verbose -keystore $KEYSTORE $TEMPAPK phrasedroid
+## zipalign the application
+zipalign 4 $TEMPAPK $RELEASE
+## Check the signature
+jarsigner -verify $RELEASE
